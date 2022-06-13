613edf6f77d82ec9125abbc56e7c1247e4b086aa
Alexsandr Voronkov <voronkov_sasha2908@mail.ru>
2022-06-13 16:03:09 +0500

    Add task view to the index

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    objects = models.Manager()
+
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField()
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,9 +1,22 @@
+import datetime
+
 from django.shortcuts import render
+
+from firstapp.models import Task
 
 
 def index(request):
-    return render(request, "index.html")
+    tasks = Task.objects.all()
+
+    result = []
+
+    for task in tasks:
+        result.append(task.title)
+
+    return render(request, "index.html", context={"tasks": result})
 
 
 def add(request):
+    currentDate = datetime.datetime.now()
+
     return render(request, "add.html")
