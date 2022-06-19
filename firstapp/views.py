97ec8bc357cfa69b8050eed618d671a35be009f5
Alexsandr Voronkov <voronkov_sasha2908@mail.ru>
2022-06-19 11:56:05 +0500

    Add redirect to index

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,4 @@
-
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
 from firstapp.models import Task
@@ -12,20 +12,26 @@ def index(request):
     result = {}
 
     for task in tasks:
-        result[task.date] = task.title
+        result[task.date] = task.title  # в дальнейшем изменить вывод по времени( сортировать задачи по времени)
 
     return render(request, "index.html", context={"tasks": result})
 
 
 def add(request):
     userTask = UserTask()
+
     if request.method == "POST":
         userTask = UserTask(request.POST)
         if userTask.is_valid():
             dateAdd = userTask.cleaned_data["dateAdd"]
             timeAdd = userTask.cleaned_data["timeAdd"]
             taskAdd = userTask.cleaned_data["taskAdd"]
-            #return HttpResponse("<h2>Задача '{0}', добавлена на дату {2} в {1} часов".format(taskAdd, timeAdd, dateAdd))
-            value_for_update = {"title": taskAdd, "date": dateAdd, "time": timeAdd} #значения для добавления взятые выше
-            newTask = Task.objects.update_or_create(defaults=value_for_update, id=None)#добавление новой задачи даты и время
+
+            # значения для добавления взятые выше
+            value_for_update = {"title": taskAdd, "date": dateAdd, "time": timeAdd}
+
+            # добавление новой задачи даты и время
+            newTask = Task.objects.update_or_create(defaults=value_for_update, id=None)
+            return HttpResponseRedirect("/index")
+
     return render(request, "add.html", {"form": userTask})
